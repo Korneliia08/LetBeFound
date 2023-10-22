cbf665e2d3ab18b4917944e7d58db1100fb7e6d6
Korneliia <korneliiaPositive@gmail.com>
2023-10-23 00:34:22 +0300

    add styles

--- scripts/header.js
+++ scripts/header.js
@@ -1,9 +1,7 @@
 const blockMenu = document.querySelector(".blockMenu")
 document.addEventListener("scroll", (event) => {
-
     blockMenu.classList.remove("moveNav")
     if (window.scrollY != 0) {
         blockMenu.classList.add("moveNav")
-
     }
 })
